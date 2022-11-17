7b441c772e73bba6b20df615c3c9d08c8360de8f
Riley Housden <riley.t.housden@gmail.com>
2022-11-17 00:01:39 -0500

    Restructure exceptions

--- yabu_cmd/exceptions/exceptions.py
+++ yabu_cmd/exceptions/exceptions.py
@@ -1,13 +1,19 @@
 from pathlib import Path
 
 class BackupException(Exception):
+    def __init__(self, msg) -> None:
+        super().__init__(msg)
+        self.message = msg
     pass
 
 class PresetNotFoundException(BackupException):
     pass
 
 class InvalidPresetConfig(BackupException):
-    def __init__(self, msg) -> None:
-        super().__init__(msg)
-        self.message = msg
+    pass
+
+class UnsupportedFormatException(BackupException):
+    pass
+
+class NotABackupException(BackupException):
     pass
