4fb95e355f8090cfae71acff3901fbf78b438320
Riley Housden <riley.t.housden@gmail.com>
2022-11-18 01:21:45 -0500

    Add __str__ to backup objects

--- yabu_cmd/controller/backup.py
+++ yabu_cmd/controller/backup.py
@@ -4,12 +4,23 @@ from datetime import datetime
 
 __all__ = ['Backup']
 
-@dataclass(frozen=True)
+@dataclass(frozen=True, repr=True)
 class Backup:
     name: str = None
     path: Path = None
     date_format: str = None
-    separator: str = None
+    name_separator: str = None
     target: Path = None
     date: datetime = None
-    md5_hash: str = None
+    content_hash: str = None
+
+    def __str__(self) -> str:
+        output = "Backup:"
+        output += f"\n\tname: {self.name}"
+        output += f"\n\tpath: {self.path}"
+        output += f"\n\tdate_format: {self.date_format}"
+        output += f"\n\tname_separator: {self.name_separator}"
+        output += f"\n\ttarget: {self.target}"
+        output += f"\n\tdate: {self.date}"
+        output += f"\n\tcontent_hash: {self.content_hash}"
+        return output
