d62e52bfa728f8b31faac8d90812f19e46c59573
Riley Housden <riley.t.housden@gmail.com>
2022-11-18 06:35:37 -0500

    restructure backup tests

---
+++ tests/test_backup.py
@@ -0,0 +1,20 @@
+from yabu_cmd import PresetManager, BackupManager
+
+class TestBackup:
+    def test_backup_str(self, setup_create_backups):
+        preset_manager = PresetManager(setup_create_backups)
+        presets = preset_manager.get_presets()
+        backup_manager = BackupManager()
+        backup_manager.create_backups(presets[0])
+        for backup in backup_manager.get_backups(presets[0]):
+            backup_str = str(backup)
+            print(repr(backup_str))
+            correct_output = f"""Backup:
+\tname: folder
+\tpath: C:\\Users\\Riley\\.storage\\programs\\backup_cmd\\temp\\{backup.name}{backup.name_separator}{backup.date.strftime(backup.date_format)}.zip
+\tdate_format: %d_%m_%y__%H%M%S%f
+\tname_separator: -
+\ttarget: C:\\Users\\Riley\\.storage\\programs\\backup_cmd\\temp\\folder
+\tdate: {backup.date}
+\tcontent_hash: {backup.content_hash}"""
+            assert backup_str == correct_output
