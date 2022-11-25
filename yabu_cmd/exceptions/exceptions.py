68ae03f102debb16f15fe767745d323abaa1c575
Riley Housden <riley.t.housden@gmail.com>
2022-11-24 22:24:15 -0500

    Refactor Exceptions contains two exception categories, PresetException and BackupException both exception categories inherit from YabuException added DestinationNotFoundException added TargetNotFoundException added BackupHashException

--- yabu_cmd/exceptions/exceptions.py
+++ yabu_cmd/exceptions/exceptions.py
@@ -1,19 +1,37 @@
 from pathlib import Path
 
-class BackupException(Exception):
+class YabuException(Exception):
     def __init__(self, msg) -> None:
         super().__init__(msg)
         self.message = msg
     pass
 
-class PresetNotFoundException(BackupException):
+class NotABackupException(YabuException):
     pass
 
-class InvalidPresetConfig(BackupException):
+class PresetException(YabuException):
+    pass
+
+class BackupException(YabuException):
+    def __init__(self, msg, target: Path, destination: Path) -> None:
+        self.target = target
+        self.destination = destination
+        super().__init__(msg)
+
+class PresetNotFoundException(PresetException):
+    pass
+
+class InvalidPresetConfig(PresetException):
     pass
 
 class UnsupportedFormatException(BackupException):
     pass
 
-class NotABackupException(BackupException):
+class BackupHashException(BackupException):
+    pass
+
+class DestinationNotFoundException(BackupException):
+    pass
+
+class TargetNotFoundException(BackupException):
     pass
