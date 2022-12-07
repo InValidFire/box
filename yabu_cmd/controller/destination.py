a8756caa697ad63d5105259a23c1dd0a3fb6fbdb
Riley Housden <riley.t.housden@gmail.com>
2022-12-07 18:37:56 -0500

    add documentation for destination object.

--- yabu_cmd/controller/destination.py
+++ yabu_cmd/controller/destination.py
@@ -31,21 +31,40 @@ class Destination:
 
     @property
     def path(self) -> Path:
+        """The destination path, or where the backup archive will be stored. This should be a folder."""
         return self._path
 
     @path.setter
     def path(self, new_path: Path) -> Path:
+        """The destination path, or where the backup archive will be stored. This should be a folder."""
         if not isinstance(new_path, Path):
             raise TypeError(new_path)
+        if not new_path.is_dir():
+            raise ValueError(new_path)
         else:
             self._path = new_path
 
     @property
-    def max_backup_count(self):
+    def max_backup_count(self) -> int:
+        """The max backup count, or how many backups should be stored in this destination before the oldest backups are deleted. 
+        This can be overwritten with the -k argument.
+
+        Returns:
+            int: The max backup count.
+        """
         return self._max_backup_count
 
     @max_backup_count.setter
     def max_backup_count(self, new_max_backup_count):
+        """The max backup count, or how many backups should be stored in this destination before the oldest backups are deleted.
+
+        Args:
+            new_max_backup_count (object): The potential new value for the max_backup_count.
+
+        Raises:
+            TypeError: If the new_max_backup_count is not an integer.
+            ValueError: If the new_max_backup_count is not a positive number.
+        """
         if not isinstance(new_max_backup_count, int):
             raise TypeError(new_max_backup_count)
         elif not new_max_backup_count > 0:
@@ -55,10 +74,23 @@ class Destination:
 
     @property
     def date_format(self) -> str:
+        """The date format to use for new backups. This should use datetime notation.
+
+        Returns:
+            str: The date format string
+        """
         return self._date_format
 
     @date_format.setter
     def date_format(self, new_date_format: str):
+        """The date format to use for new backups. This should use datetime notation.
+
+        Args:
+            new_date_format (str): The potential new value for the date_format.
+
+        Raises:
+            TypeError: If the new_date_format is not a string.
+        """
         if not isinstance(new_date_format, str):
             raise TypeError(new_date_format)
         else:
@@ -66,21 +98,49 @@ class Destination:
 
     @property
     def name_separator(self) -> str:
+        """The separator string used to separate the backup name from the backup date in the archive name.
+
+        Returns:
+            str: The name separator.
+        """
         return self._name_separator
 
     @name_separator.setter
-    def name_separator(self, new_separator):
+    def name_separator(self, new_separator: str):
+        """The name separator string used to separate the backup name from the backup date in the archive name.
+
+        Args:
+            new_separator (str): The potential new value for the name_separator.
+
+        Raises:
+            TypeError: If the new_separator is not a string.
+        """
         if not isinstance(new_separator, str):
             raise TypeError(new_separator)
         else:
             self._name_separator = new_separator
 
     @property
-    def file_format(self):
+    def file_format(self) -> str:
+        """The file format to create the archive in. Current supported formats are:
+        - zip
+
+        Returns:
+            str: The file format.
+        """
         return self._file_format
 
     @file_format.setter
-    def file_format(self, new_file_format):
+    def file_format(self, new_file_format: str):
+        """The file format to create the archive in. Current supported formats are:
+        - zip
+
+        Args:
+            new_file_format (str): The potential new value for the file format.
+
+        Raises:
+            ValueError: If the new_file_format
+        """
         if new_file_format not in VALID_FILE_FORMATS:
             raise ValueError(new_file_format)
         else:
