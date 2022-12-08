403a48a6cac1d1d40e2d98b432ff7d0a7856090a
Riley Housden <riley.t.housden@gmail.com>
2022-12-08 00:39:05 -0500

    add documentation for the preset object.

--- yabu_cmd/controller/preset.py
+++ yabu_cmd/controller/preset.py
@@ -37,29 +37,70 @@ class Preset:
 
     @property
     def name(self) -> str:
+        """The name of the preset. Used to identify the preset in the command line tool.
+
+        Returns:
+            str: The preset name.
+        """
         return self._name
 
     @name.setter
     def name(self, new_name) -> None:
+        """The name of the preset. Used to identify the preset in the command line tool.
+
+        Args:
+            new_name (str): The potential new value for the name.
+
+        Raises:
+            TypeError: If the new_name is not a string.
+        """
         if not isinstance(new_name, str):
             raise TypeError(new_name)
         else:
             self._name = new_name
 
     def add_target(self, target: Path):
+        """Add a target to the list of targets for the preset. 
+        This is the recommended way to add targets to the list, as it implements type checking to the values before they're added.
+
+        Args:
+            target (Path): The target to add to the list.
+
+        Raises:
+            TypeError: If the given target is not a Path object.
+        """
         if isinstance(target, Path):
             self._targets.append(target)
         else:
             raise TypeError
 
     def add_destination(self, destination: Destination):
+        """Add a destination to the list of destinations for the preset.
+        This is the recommended way to add destinations to the list, as it implements type checking to the values before they're added.
+
+        Args:
+            destination (Destination): The destination to add to the list.
+
+        Raises:
+            TypeError: If the given destination is not a Destination object.
+        """
         if isinstance(destination, Destination):
             self._destinations.append(destination)
         else:
             raise TypeError(destination)
 
     def remove_target(self, target: Path):
+        """Remove a target from the list of targets for the preset.
+
+        Args:
+            target (Path): The target path to remove.
+        """
         self._targets.remove(target)
 
     def remove_destination(self, destination: Destination):
+        """Remove a Destination from the list of destinations for the preset.
+
+        Args:
+            destination (Destination): The Destination object to remove.
+        """
         self._destinations.remove(destination)
